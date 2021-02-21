d36c5324ca5a77ffaf1bd30871ef9d37e9eecdac
Siarhei Piatrashka <sergei.petrashko8@gmail.com>
2021-02-21 23:18:48 +0300

    + Webpack packages.

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+/** 
+ * Common webpack config.
+ * @type {import('webpack').Configuration} */
+const config = {
+
+};
+
+/**
+ * Webpack config.
+ */
+module.exports = () => {
+
+    return config;
+};
